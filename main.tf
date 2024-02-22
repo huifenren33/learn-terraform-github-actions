5c0220b27bee29526358d17a12e37cf611ea1a9f
Huifen Ren <huifenren33@gmail.com>
2024-02-21 21:44:23 -0500

    Point backend to correct Terraform configure organization and workspace

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "HR-terraform-demo"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "hr-github-training"
     }
   }
 }
